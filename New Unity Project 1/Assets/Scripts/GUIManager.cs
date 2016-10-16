ea80acb05833f796d8cfc3a47bcfff0bdf0ee6bf
gamerblox <gamerblox10@gmail.com>
2016-10-15 18:18:47 -0700

    GUI Update
    Scott

---
+++ New Unity Project 1/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour {
+
+    public Text score_text_object;
+    public GameObject player_object;
+
+    string score_text = "Score: ";
+
+	void Start ()
+    {
+        score_text_object.text = score_text + "0";
+
+	}
+	
+	void Update ()
+    {
+        score_text_object.text = score_text + player_object.GetComponent<playerManager>().points;
+
+    }
+
+}
